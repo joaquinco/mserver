0ced6d01b6612275eec319d40f6168aa621a29c8
Joaquin Correa <joaquincorrea333@gmail.com>
2018-10-06 15:44:20 -0300

    improve new volume usage mobile

--- web/src/components/VolumeV2.vue
+++ web/src/components/VolumeV2.vue
@@ -1,8 +1,9 @@
 <template>
     <div class='volume'>
       <span class='icon icon--sm volume__toggle' @click='toggle()'/>
-      <div v-if="isOpen" class='volume__control'>
+      <div v-if="isOpen" class='volume__control'  @touchmove.stop>
         <RangeInput :value='value' :max='max' :min='min' @change='onValueChange'/>
+        <span class='volume__value'>{{value}}%</span>
       </div>
     </div>
 </template>
@@ -55,16 +56,23 @@ export default {
     position: relative;
 
     &__control {
-      padding: 15px 10px 10px 10px;
+      display: flex;
+      flex-direction: row;
+      padding: 15px 35px 15px 20px;
       position: absolute;
-      width: 200px;
-      bottom: 100px;
-      left: -90px;
+      width: 240px;
+      bottom: 120px;
+      left: -112px;
       background: white;
       box-shadow: 0px 0px 5px #919191;
-      -ms-transform: rotate(270deg); /* IE 9 */
-      -webkit-transform: rotate(270deg); /* Safari */
       transform: rotate(270deg);
+    }
+
+    &__value {
+      position: absolute;
+      right: 0;
+      top: 57%;
+      transform: rotate(90deg) translateX(-50%);
     }
 
     &__toggle {
